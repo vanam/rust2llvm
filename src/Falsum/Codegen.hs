6452b563fda0874e95e6ddbf5e79e457cf28dd1a
Goheeca <goheeca@gmail.com>
2016-12-10 15:58:45 +0100

    Partial LLVM IR for 01_simple

--- src/Falsum/Codegen.hs
+++ src/Falsum/Codegen.hs
@@ -1,7 +1,16 @@
 module Falsum.Codegen where
 
+import           Data.Word
 import           Falsum.AST
 import qualified LLVM.General.AST                        as AST
+import           LLVM.General.AST.AddrSpace
+import qualified LLVM.General.AST.CallingConvention      as CC
+import qualified LLVM.General.AST.Instruction            as I
+import qualified LLVM.General.AST.Linkage                as L
+import qualified LLVM.General.AST.Type                   as T
+import qualified LLVM.General.AST.Visibility             as V
+-- import qualified LLVM.General.AST.DLL as DLL import qualified LLVM.General.AST.ThreadLocalStorage
+-- as TLS
 import qualified LLVM.General.AST.Constant               as C
 import qualified LLVM.General.AST.Float                  as F
 import qualified LLVM.General.AST.FloatingPointPredicate as FP
@@ -29,8 +38,89 @@ simple = Program
                   (VarLet (VarSymbol "b" Int) (IExpr (IVar (ConstSymbol "ANSWER" Int (IntVal 42)))))
               ])
 
+defaultAlignment :: Word32
+defaultAlignment = 4
+
+i32Lit :: Integer -> C.Constant
+i32Lit = C.Int 32
+
+defaultAddrSpace :: AddrSpace
+defaultAddrSpace = AddrSpace 0
+
+simple_ANSWER = AST.GlobalVariable -- https://github.com/bscarlet/llvm-general/blob/llvm-3.5/llvm-general-pure/src/LLVM/General/AST/Global.hs#L21
+
+                  (AST.Name "ANSWER")
+                  L.Internal
+                  V.Default
+                  Nothing
+                  Nothing
+                  defaultAddrSpace
+                  True
+                  True
+                  T.i32
+                  (Just $ i32Lit 42)
+                  Nothing
+                  Nothing
+                  defaultAlignment
+
+simple_M = AST.GlobalVariable -- https://github.com/bscarlet/llvm-general/blob/llvm-3.5/llvm-general-pure/src/LLVM/General/AST/Global.hs#L21
+
+             (AST.Name "M")
+             L.Internal
+             V.Default
+             Nothing
+             Nothing
+             defaultAddrSpace
+             False
+             False
+             T.i32
+             (Just $ i32Lit 42)
+             Nothing
+             Nothing
+             defaultAlignment
+
+body :: [I.Named I.Instruction] -> (I.Named I.Terminator) -> [AST.BasicBlock]
+body instructions terminator = [AST.BasicBlock -- https://github.com/bscarlet/llvm-general/blob/llvm-3.5/llvm-general-pure/src/LLVM/General/AST/Global.hs#L74
+
+                                  (AST.Name "")
+                                  instructions
+                                  terminator]
+
+-- TODO
+simple_foo :: AST.Global
+simple_foo = AST.Function -- https://github.com/bscarlet/llvm-general/blob/llvm-3.5/llvm-general-pure/src/LLVM/General/AST/Global.hs#L48
+
+               L.Internal
+               V.Default
+               Nothing
+               CC.C
+               []
+               T.void
+               (AST.Name "foo")
+               ([], False)
+               []
+               Nothing
+               Nothing
+               defaultAlignment
+               Nothing
+               Nothing
+               (body []
+                  (I.Do -- https://github.com/bscarlet/llvm-general/blob/llvm-3.5/llvm-general-pure/src/LLVM/General/AST/Instruction.hs#L415
+
+                     (I.Ret -- https://github.com/bscarlet/llvm-general/blob/llvm-3.5/llvm-general-pure/src/LLVM/General/AST/Instruction.hs#L24
+
+                        Nothing
+                        [])))
+
+-- TODO
+simple_main = undefined
+
+topLevelDefs :: [AST.Definition]
+topLevelDefs = fmap AST.GlobalDefinition $
+  [simple_ANSWER, simple_M, simple_foo]
+
 moduleInAST :: AST.Module
-moduleInAST = AST.defaultModule
+moduleInAST = AST.Module "01_simple" Nothing Nothing topLevelDefs
 
 {-main =
     LLVMCtx.withContext $ \ctx ->
