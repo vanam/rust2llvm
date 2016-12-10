f175f1b6cb054db39fb2dc3925b4286ac32c1fd1
Goheeca <goheeca@gmail.com>
2016-12-10 14:22:40 +0100

    Generating empty LLVM IR

--- src/Falsum/Codegen.hs
+++ src/Falsum/Codegen.hs
@@ -5,6 +5,15 @@ import qualified LLVM.General.AST                        as AST
 import qualified LLVM.General.AST.Constant               as C
 import qualified LLVM.General.AST.Float                  as F
 import qualified LLVM.General.AST.FloatingPointPredicate as FP
+import qualified LLVM.General.Context                    as CTX
+import qualified LLVM.General.Module                     as MOD
+import qualified LLVM.General.PrettyPrint                as PP
+
+import           Control.Monad
+import           Control.Monad.Trans.Except
+
+liftError :: ExceptT String IO a -> IO a
+liftError = runExceptT >=> either fail return
 
 simple :: Program
 simple = Program
@@ -20,4 +29,23 @@ simple = Program
                   (VarLet (VarSymbol "b" Int) (IExpr (IVar (ConstSymbol "ANSWER" Int (IntVal 42)))))
               ])
 
-main = putStrLn "hello world"
+moduleInAST :: AST.Module
+moduleInAST = AST.defaultModule
+
+{-main =
+    LLVMCtx.withContext $ \ctx ->
+        liftError $ LLVMMod.withModuleFromBitcode ctx file $ \mod -> do
+            ast <- LLVMMod.moduleAST mod
+            putStrLn $ LLVMPP.showPretty ast
+            liftError $ LLVMMod.withModuleFromAST ctx ast$ \mod -> do
+                putStrLn "Success!"
+
+    where
+        file = LLVMMod.File "Rx-linked-cg.bc"-}
+asm :: IO String
+asm = CTX.withContext $ \ctx -> liftError $ MOD.withModuleFromAST ctx moduleInAST $ \moduleInLowLevel -> do
+  MOD.moduleLLVMAssembly moduleInLowLevel
+
+main = do
+  llvmIR <- asm
+  putStrLn llvmIR
