56b3cf75ede1ef65689a105b58c55be7e4eca056
Martin Váňa <vanam@students.zcu.cz>
2017-01-08 10:22:36 +0100

    Fixed falsum and run_examples.sh bash scripts.

--- run_examples.sh
+++ run_examples.sh
@@ -3,22 +3,28 @@
 FILES=./examples/*.rs
 BIN=tmp
 
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 for f in $FILES
 do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  if [ ! -f "$f" ]; then
+  echo -e "${GREEN}falsum: Processing '$f' file...${NC}"
+
+  if [ ! -f "$f" ]
+  then
     continue
   fi
 
   ./falsum -o "$BIN" "$f"
 
-  if [ $? -eq 0 ]; then
+  if [ $? -eq 0 ]
+  then
     ./"$BIN"
   fi
 
 done
 
-if [ -f "$BIN" ]; then
+if [ -f "$BIN" ]
+then
   rm "$BIN"
 fi
