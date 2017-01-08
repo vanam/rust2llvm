97a56a13b617bbb00dd4797f37d53e70624f487d
Goheeca <goheeca@gmail.com>
2017-01-08 18:23:25 +0100

    using stderr

--- app/Main.hs
+++ app/Main.hs
@@ -3,63 +3,24 @@ module Main where
 import           Falsum.Codegen
 import           Falsum.Parser
 import           Falsum.Transform
+import           System.IO
 
--- import System.Directory (doesFileExist) import System.IO (hFlush, stdout)
-{-
-yesno :: String -> IO Bool
-yesno prompt = do
-  putStr $ prompt ++ " y/n: "
-  hFlush stdout
-  str <- getLine
-  case str of
-    "y" -> return True
-    "n" -> return False
-    _ -> do
-      putStrLn "Invalid input."
-      yesno prompt
-
-main :: IO ()
-main = do
-  putStr "Enter a filename to start tokenization & parsing: "
-  hFlush stdout
-  filename <- getLine
-  exists <- doesFileExist filename
-  if exists
-    then do
-      sourceCode <- readFile filename
-      parsed <- return $ tokenizeParse filename $ sourceCode
-      case parsed of
-        Left lexerError -> do
-          putStrLn "Lexer fails:"
-          putStrLn . show $ lexerError
-        Right (Left parserError) -> do
-          putStrLn "Parser fails:"
-          putStrLn . show $ parserError
-        Right (Right ast) -> do
-          randomSuffix <- randomString 20
-          codegen filename (transformProgram ("main_" ++ randomSuffix) ast)
-    else putStrLn "The file doesn't exist."
-  continue <- yesno "Continue with another file"
-  if continue
-    then main
-    else return ()
--}
 main :: IO ()
 main = do
   sourceCode <- getContents
   parsed <- return $ tokenizeParse "stdin" $ sourceCode
   case parsed of
     Left lexerError -> do
-      putStrLn "Lexer fails:"
-      putStrLn . show $ lexerError
+      hPutStrLn stderr "Lexer fails:"
+      hPutStrLn stderr . show $ lexerError
     Right (Left parserError) -> do
-      putStrLn "Parser fails:"
-      putStrLn . show $ parserError
+      hPutStrLn stderr "Parser fails:"
+      hPutStrLn stderr . show $ parserError
     Right (Right ast) -> do
-      putStrLn "Parser AST:"
-      putStrLn . show $ ast
+      hPutStrLn stderr "Parser AST:"
+      hPutStrLn stderr . show $ ast
       transformedAst <- return $ transformProgram ast
-      putStrLn "" >> putStrLn "Transformed AST:"
-      putStrLn . show $ transformedAst
-      putStrLn "" >> putStrLn "Codegen:"
+      hPutStrLn stderr ""
+      hPutStrLn stderr "Transformed AST:"
+      hPutStrLn stderr . show $ transformedAst
       codegen "stdin" transformedAst
